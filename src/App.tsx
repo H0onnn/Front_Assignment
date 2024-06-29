b5313a4eb65c853eeb434425289136f307cf53fd
H0onnn <dukei2017@naver.com>
2024-06-29 20:33:22 +0900

    refactor : App 컴포넌트 로직 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,32 @@
-import React, { useState, useCallback } from "react"
-import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd"
+import { useDragAndDrop } from "hooks/useDradAndDrop"
+import { DragDropContext, Droppable } from "react-beautiful-dnd"
+import { DraggableList } from "components/drag-column"
+import { getListStyle } from "utils"
 
-interface Item {
-  id: string
-  content: string
-}
-
-function App() {
-  const getItems = (count: number): Item[] =>
-    Array.from({ length: count }, (_, k) => k).map((k) => ({
-      id: `item-${k}`,
-      content: `item ${k}`,
-    }))
-
-  const [items, setItems] = useState<Item[]>(getItems(10))
-
-  const reorder = (list: Item[], startIndex: number, endIndex: number): Item[] => {
-    const result = Array.from(list)
-    const [removed] = result.splice(startIndex, 1)
-    result.splice(endIndex, 0, removed)
-    return result
-  }
-
-  const onDragEnd = useCallback(
-    (result: DropResult) => {
-      if (!result.destination) {
-        return
-      }
-
-      const newItems = reorder(items, result.source.index, result.destination.index)
-
-      setItems(newItems)
-    },
-    [items],
-  )
+const App = () => {
+  const { onDragEnd, columns } = useDragAndDrop()
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided, snapshot) => (
-          <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            style={getListStyle(snapshot.isDraggingOver)}
-          >
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided, snapshot) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-                  >
-                    {item.content}
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+      <div className="flex justify-around">
+        {columns.map((column) => (
+          <Droppable key={column.id} droppableId={column.id}>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                className={getListStyle(snapshot.isDraggingOver)}
+              >
+                <h2>{column.title}</h2>
+                <DraggableList items={column.items} />
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
     </DragDropContext>
   )
 }
-
-const GRID = 8
-
-const getItemStyle = (
-  isDragging: boolean,
-  draggableStyle: React.CSSProperties | undefined,
-): React.CSSProperties => ({
-  userSelect: "none",
-  padding: GRID * 2,
-  margin: `0 0 ${GRID}px 0`,
-  background: isDragging ? "lightgreen" : "grey",
-  ...draggableStyle,
-})
-
-const getListStyle = (isDraggingOver: boolean): React.CSSProperties => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: GRID,
-  width: 250,
-})
 
 export default App
