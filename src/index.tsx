ae2074ad06b9049faf0098beaa6785280e9f9fd6
H0onnn <dukei2017@naver.com>
2024-06-30 16:16:49 +0900

    style : reset css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import App from "./App"
 import ReactDOM from "react-dom/client"
-import "./index.css"
+import "./styles/index.ts"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
