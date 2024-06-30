8763cacc9ab8245f79c33cfb3d348129d9bb16f2
H0onnn <dukei2017@naver.com>
2024-06-30 18:39:49 +0900

    style : 스타일링 추가

--- src/components/DragColumns.tsx
+++ src/components/DragColumns.tsx
@@ -1,15 +1,16 @@
 import { useDragAndDrop } from "hooks"
 import { DragDropContext, Droppable } from "react-beautiful-dnd"
 import { DraggableList } from "components/drag"
+import { ColorBadge } from "components/common/ui"
 import { getListStyle } from "utils"
 import { Column } from "types/column"
 
 export const DragColumns = () => {
-  const { columns, onDragUpdate, handleDragEnd, invalidDrop } = useDragAndDrop()
+  const { columns, onDragUpdate, onDragEnd, invalidDrop } = useDragAndDrop()
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd} onDragUpdate={onDragUpdate}>
-      <div className="flex justify-between space-x-2">
+    <DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
+      <div className="flex justify-between space-x-2 h-full">
         {columns.map((column: Column) => (
           <Droppable key={column.id} droppableId={column.id}>
             {(provided, snapshot) => (
@@ -18,7 +19,16 @@ export const DragColumns = () => {
                 {...provided.droppableProps}
                 className={getListStyle({ isDraggingOver: snapshot.isDraggingOver, invalidDrop })}
               >
-                <h2>{column.title}</h2>
+                <div className="sticky top-0 z-10 bg-inherit">
+                  <div className="flex items-center space-x-2 mb-2">
+                    <ColorBadge columnType={column.title} />
+                    <h2 className="font-semibold">{column.title}</h2>
+                    <div className="bg-gray-300 rounded-full w-6 h-4 text-center text-xs text-gray-600">
+                      {column.items.length}
+                    </div>
+                  </div>
+                  <h3 className="text-sm text-gray-500">{column.subtitle}</h3>
+                </div>
                 <DraggableList items={column.items} invalidDrop={invalidDrop} />
                 {provided.placeholder}
               </div>
--- src/utils/getStyles.ts
+++ src/utils/getStyles.ts
@@ -1,15 +1,37 @@
-import React from "react"
+import { CSSProperties } from "react"
 import { cn } from "./cn"
 
 interface GetDraggableStyleProps {
+  draggableStyle: CSSProperties | undefined
   isDragging: boolean
-  draggableStyle: React.CSSProperties | undefined
+  invalidDrop: boolean
+  isSelected: boolean
 }
 
-export const getItemStyle = ({ isDragging, draggableStyle }: GetDraggableStyleProps): string => {
-  return cn(["user-select-none p-4 mb-2", isDragging ? "bg-green-200" : "bg-white", draggableStyle])
+export const getItemStyle = ({
+  draggableStyle,
+  isDragging,
+  invalidDrop,
+  isSelected,
+}: GetDraggableStyleProps): string => {
+  return cn([
+    "user-select-none p-3 rounded-md shadow-sm relative bg-white",
+    isSelected && "bg-blue-100 border-2 border-blue-500",
+    isDragging && "bg-green-200 shadow-lg",
+    isDragging && invalidDrop && "bg-red-200 opacity-50",
+
+    draggableStyle,
+  ])
 }
 
-export const getListStyle = (isDraggingOver: boolean): string => {
-  return cn(["p-4 w-[250px] min-h-[500px]", isDraggingOver ? "bg-blue-200" : "bg-gray-200"])
+interface GetListStyleProps {
+  isDraggingOver: boolean
+  invalidDrop: boolean
+}
+
+export const getListStyle = ({ isDraggingOver, invalidDrop }: GetListStyleProps): string => {
+  return cn([
+    "p-4 w-[348px] min-h-[500px] h-full bg-gray-100 flex flex-col space-y-2 border border-solid border-gray-300 rounded-md overflow-y-auto",
+    isDraggingOver && invalidDrop && "bg-red-400",
+  ])
 }
