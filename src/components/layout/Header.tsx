b8403c5438d2652fb9bf54ac4021c929706ba830
H0onnn <dukei2017@naver.com>
2024-06-30 16:56:59 +0900

    feat : Header 추가

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="sticky top-0 z-10 bg-white shadow">
+      <div className="container mx-auto py-4">
+        <h1 className="text-2xl font-semibold">Mement AI</h1>
+      </div>
+    </header>
+  )
+}
