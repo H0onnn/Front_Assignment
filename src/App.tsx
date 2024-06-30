d4c313daa0c82fe9615143193166749bb215c4c7
H0onnn <dukei2017@naver.com>
2024-06-30 16:57:38 +0900

    refactor : App 컴포넌트 재구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { useDragAndDrop } from "hooks/useDradAndDrop"
-import { DragDropContext, Droppable } from "react-beautiful-dnd"
-import { DraggableList } from "components/drag-column"
-import { getListStyle } from "utils"
+import { SelectedItemsProvider as ContextProvider } from "context/SelectedItemContext"
+import { DragColumns } from "components/DragColumns"
+import { PageLayout } from "components/layout"
 
 const App = () => {
-  const { onDragEnd, columns } = useDragAndDrop()
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="flex justify-around">
-        {columns.map((column) => (
-          <Droppable key={column.id} droppableId={column.id}>
-            {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className={getListStyle(snapshot.isDraggingOver)}
-              >
-                <h2>{column.title}</h2>
-                <DraggableList items={column.items} />
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        ))}
-      </div>
-    </DragDropContext>
+    <PageLayout>
+      <ContextProvider>
+        <DragColumns />
+      </ContextProvider>
+    </PageLayout>
   )
 }
 
