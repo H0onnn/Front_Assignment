0aaf3d9efdbc0a3036b5364c7cf1833149998c4a
H0onnn <dukei2017@naver.com>
2024-06-30 18:40:56 +0900

    feat : 아이템 없을 때 분기 처리

--- src/components/drag/DraggableList.tsx
+++ src/components/drag/DraggableList.tsx
@@ -6,10 +6,16 @@ interface DraggableListProps {
   invalidDrop: boolean
 }
 
-export const DraggableList = ({ items, invalidDrop }: DraggableListProps) => (
-  <>
-    {items.map((item, index) => (
-      <DraggableItem key={item.id} item={item} index={index} invalidDrop={invalidDrop} />
-    ))}
-  </>
-)
+export const DraggableList = ({ items, invalidDrop }: DraggableListProps) => {
+  if (!items.length) {
+    return <div className="text-gray-500 text-sm text-center">No items</div>
+  }
+
+  return (
+    <>
+      {items.map((item, index) => (
+        <DraggableItem key={item.id} item={item} index={index} invalidDrop={invalidDrop} />
+      ))}
+    </>
+  )
+}
